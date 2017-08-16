194f1fc84daa2559e92d53c5672c6d71961c0222
kai <1079049816@qq.com>
2017-08-16 22:10:12 +0800

    新建文件

---
+++ src/main/java/com/mmall/controller/backend/CategoryManagerController.java
@@ -0,0 +1,9 @@
+package com.mmall.controller.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manager/category")
+public class CategoryManagerController {
+}
---
+++ src/main/java/com/mmall/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.mmall.service;
+
+import org.springframework.stereotype.Service;
+
+@Service("categoryService")
+public interface CategoryService {
+}
